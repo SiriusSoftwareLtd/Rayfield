9b296b43605aa3a4d904de5c8bc0ce5f72b04f61
Jenson Hirst <jenson@sirius.menu>
2026-03-31 21:22:54 +0100

    fix
    Removed player kicked event connection when not using Studio.

--- source.lua
+++ source.lua
@@ -3580,14 +3580,6 @@ function RayfieldLibrary:CreateWindow(Settings)
 		})
 	end
 
-	if not useStudio then
-		Players.LocalPlayer.Kicked:Connect(function(kickMessage)
-			sendReport("player_kicked", Settings.Name or "Unknown", {
-				kick_reason = kickMessage and tostring(kickMessage):sub(1, 256) or "",
-			})
-		end)
-	end
-
 	return Window
 end
 
